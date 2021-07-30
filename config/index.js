06ae56162bb4b4cd506690b7ebb28ca3d9db7c96
Omar Medhat <omarmedhat2004@gmail.com>
2021-07-30 16:26:37 +0800

    pdf resume

--- config/index.js
+++ config/index.js
@@ -96,10 +96,10 @@ module.exports = {
       },
     ],
     button: {
-      useFileName: true,
+      useFileName: false,
       name: "Resume",
       fileName: "OmarSolieman.pdf", // the file has to be placed inside the static folder at the root level
-      url: "", // if useFileName=false, you can set an anchor link here and use the button for navigational purposes
+      url: "https://pdfhost.io/v/2SuFbr.hI_Resume.pdf", // if useFileName=false, you can set an anchor link here and use the button for navigational purposes
     },
   },
   footerLinks: [
