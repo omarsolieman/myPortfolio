1426fc0d50003ae319bc7346dc3a710bc508d667
Omar Medhat <omarmedhat2004@gmail.com>
2021-09-12 03:58:12 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -50,7 +50,7 @@ module.exports = {
     "https://api.rss2json.com/v1/api.json?rss_url=https%3A%2F%2Fmedium.com%2Ffeed%2F%40omarsolieman",
   //rssFeed: "https://api.rss2json.com/v1/api.json?rss_url=https%3A%2F%2Fmedium.com%2Ffeed%2F%40omarsolieman",
 
-  shownArticles: 3,
+  shownArticles: 2,
 
   //-- SOCIAL MEDIA SETTINGS -----
   // There are icons available for the following platforms:
